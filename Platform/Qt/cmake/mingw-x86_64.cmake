736be8937f11787490f8f84da809097c97bd7b0c
gitlares <57019432+gitlares@users.noreply.github.com>
2026-08-17 19:46:34 -0600

    qt: include mingw-x86_64.cmake toolchain file + refresh Linux screenshot

---
+++ Platform/Qt/cmake/mingw-x86_64.cmake
@@ -0,0 +1,12 @@
+# Cross-compile toolchain: Windows x86_64 via mingw-w64 on Linux.
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER  x86_64-w64-mingw32-windres)
+set(CMAKE_AR           x86_64-w64-mingw32-ar)
+set(CMAKE_RANLIB       x86_64-w64-mingw32-ranlib)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
